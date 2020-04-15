ab23c1cc70a07a27b13b3f8fc1054501b0582b7c
Zhuoyu Yuan <yuan.597@ufl.edu>
2020-04-15 11:10:47 -0400

    h

--- server/server.js
+++ server/server.js
@@ -39,11 +39,11 @@ connectDB().then(() => {
   // Serve static assets in production
   if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.use(express.static(path.join('client/build')));
 
     // Handle React routing, return all requests to React app
     app.get('*', function (req, res) {
-      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
     });
   }
 
